56822735121d487483735bfd927bf365ba4a1d1d
Christian Sauer <sauerc@gmail.com>
2015-09-13 21:54:56 -0400

    Don't serve google tracker in debug mode

--- project/settings/defaults.py
+++ project/settings/defaults.py
@@ -9,6 +9,11 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = (
+    '0.0.0.0',
+    '127.0.0.1',
+)
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
