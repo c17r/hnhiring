6dfeb64ef611f0e26ea5e803fd9ae34da02710c4
Christian Sauer <sauerc@gmail.com>
2017-09-05 22:17:09 -0400

    Enhanced Filtering
    Enable/Disable a particular filter temporarily.

--- app/static/app/js/hnhiring.js
+++ app/static/app/js/hnhiring.js
@@ -2,21 +2,25 @@ var FilterLibrary = (function($) {
 
     function create(cbAdd, cbRemove, cbFilter) {
 
-        var filters = [];
+        function emptyData() {
+            return {};
+        }
+
+        var filters = emptyData();
 
         function add(newFilter) {
             if (findExisting(newFilter))
                 return;
-            filters.push(newFilter);
+            filters[newFilter] = 1;
             setStorage();
-            cbAdd(newFilter);
+            cbAdd(newFilter, 1);
             cbFilter(filters);
         }
 
         function remove(oldFilter) {
             if (!findExisting(oldFilter))
                 return;
-            removeEntry(oldFilter);
+            delete filters[oldFilter];
             setStorage();
             cbRemove(oldFilter);
             cbFilter(filters);
@@ -24,20 +28,26 @@ var FilterLibrary = (function($) {
 
         function load() {
             getStorage();
-            $.each(filters, function(i, v) {
-                cbAdd(v);
+            $.each(filters, function(key, enabled) {
+                cbAdd(key, enabled);
             });
             cbFilter(filters);
         }
 
         function clear() {
-            var old = filters.slice();
-
-            filters = [];
-            setStorage();
-            $.each(old, function(i, v) {
-                cbRemove(v);
+            $.each(filters, function(key, enabled) {
+                cbRemove(key);
             });
+            filters = emptyData();
+            setStorage();
+            cbFilter(filters);
+        }
+
+        function change(filter, enabled) {
+            if (!findExisting(filter))
+                return;
+            filters[filter] = enabled
+            setStorage();
             cbFilter(filters);
         }
 
@@ -45,20 +55,12 @@ var FilterLibrary = (function($) {
             add: add,
             remove: remove,
             load: load,
-            clear: clear
+            clear: clear,
+            change: change
         };
 
         function findExisting(needle) {
-            return $.inArray(needle, filters) != -1;
-        }
-
-        function removeEntry(needle) {
-            var idx = $.inArray(needle, filters);
-
-            if (idx == -1)
-                return;
-
-            filters.splice(idx, 1);
+            return $.inArray(needle, Object.keys(filters)) !== -1;
         }
 
         function setStorage() {
@@ -69,7 +71,7 @@ var FilterLibrary = (function($) {
         function getStorage() {
             var storage = $.localStorage;
             if (storage.isEmpty('filters'))
-                filters = [];
+                filters = emptyData();
             else
                 filters = storage.get('filters');
         }
@@ -91,7 +93,7 @@ $(document).on('ready', function() {
         $months = $topHat.find('#months'),
         $filter = $topHat.find('#filter'),
         $filters = $('#filters'),
-        $filterList = $filters.find('ul'),
+        $filterTable = $filters.find('table'),
         $entries = $('#entries'),
         $entriesList = $entries.find('ul'),
         $counts = $('#counts');
@@ -126,7 +128,7 @@ $(document).on('ready', function() {
 
     $filter.find('#add_filter').on('click', processAdd);
     $filter.find('input').on('keyup', function(e) {
-        if (e.keyCode == 13)
+        if (e.keyCode === 13)
             processAdd(e);
     });
     function processAdd(e) {
@@ -139,11 +141,18 @@ $(document).on('ready', function() {
         filters.add(filter);
     }
 
-    $filterList.on('click', '.remove_filter', function(e) {
+    $filterTable.on('click', '.remove_filter', function(e) {
         e.preventDefault();
 
-        var remove = $(this).parents("li").attr('data');
+        var remove = $(this).parents("tr").attr('data');
         filters.remove(remove);
+    });
+
+    $filterTable.on('click', ':checkbox', function(e) {
+        var enabled = this.checked ? 1 : 0,
+            key = $(this).parents("tr").attr('data');
+
+        filters.change(key, enabled);
     });
 
     var filters = FilterLibrary.create(
@@ -151,21 +160,32 @@ $(document).on('ready', function() {
         removeFilter,
         filterWords);
 
-    function addFilter(text) {
-        var $li = $('<li/>');
+    function addFilter(text, enabled) {
+        var $tr = $('<tr/>'),
+            checkedText = enabled === 1 ? ' checked' : '',
+            $checkBox = $('<td/>'),
+            $textBox = $('<td/>'),
+            $buttonBox = $('<td/>');
 
-        $li.attr('data', text);
-        $li.html("&nbsp;<button class='remove_filter'>-</button>&nbsp;" + text);
-        $filterList.append($li);
+        $tr.attr('data', text);
+
+        $checkBox.html('<input type="checkbox"' + checkedText + '>');
+        $textBox.html(text);
+        $buttonBox.html('<button class="remove_filter">-</button>')
+
+        $tr.append($checkBox);
+        $tr.append($textBox);
+        $tr.append($buttonBox);
+        $filterTable.append($tr);
     }
 
     function removeFilter(text) {
-        $filterList.find('li').each(function(idx, li) {
-            var $li = $(li),
-                data = $li.attr('data');
+        $filterTable.find('tr').each(function(idx, tr) {
+            var $tr = $(tr),
+                data = $tr.attr('data');
 
-            if (data == text) {
-                $li.remove();
+            if (data === text) {
+                $tr.remove();
                 return false;
             }
         });
@@ -175,8 +195,9 @@ $(document).on('ready', function() {
         var needles = [],
             cssClass = 'row1';
         
-        $.each(filters, function(idx, text) {
-           needles.push(new RegExp(text, 'gi')); 
+        $.each(filters, function(text, enabled) {
+            if (enabled === 1)
+                needles.push(new RegExp(text, 'gi'));
         });
         
         $entriesList.find("li:visible .content").highlightRegex();
@@ -196,7 +217,7 @@ $(document).on('ready', function() {
             if (found) {
                 elem.show();
                 elem.addClass(cssClass);
-                cssClass = (cssClass == 'row1') ? 'row2' : 'row1';
+                cssClass = (cssClass === 'row1') ? 'row2' : 'row1';
             } else {
                 elem.hide();
             }
