bba3e24440b854d9c438ae1c3e3c5cfb110629f2
Christian Sauer <sauerc@gmail.com>
2023-07-01 15:54:54 -0400

    remove hackernews-python lib - causing build issues - easy enough to just copy/paste into our own file

--- app/management/algolia.py
+++ app/management/algolia.py
@@ -2,7 +2,7 @@ import re
 from datetime import datetime, timezone
 from typing import Optional, List, Tuple, Iterator
 import requests
-from hackernews import HackerNews
+from .wrapper import HackerNews
 from . import hackernews as old
 
 _date_re = re.compile(r"(\w+ \d{4})")
@@ -30,13 +30,10 @@ def get_data(hn_id: int) -> Iterator[Tuple[int, str, datetime]]:
     r = requests.get(f"https://hn.algolia.com/api/v1/items/{hn_id}")
     r.raise_for_status()
     post = r.json()
-    dead = 0
     for entry in post["children"]:
         if entry["text"] is None or entry["text"] == "<p>[dead]</p>" or entry["text"] == "<p>[flagged]</p>":
-            dead += 1
             continue
         yield entry["id"], entry["text"], datetime.fromtimestamp(entry["created_at_i"], tz=timezone.utc)
-    print("dead>", dead)
 
 
 def process_job_feed() -> Iterator[dict]:
